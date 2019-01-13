66c68d845976665077c3cd85bd6a0e57519bcd16
Generosa Litton <glcodeworks@gmail.com>
2019-01-12 23:06:18 -0800

    added more stuff

--- search.py
+++ search.py
@@ -3,4 +3,10 @@ browser = webdriver.Chrome()
 
 # Get the page you'd like to open
 browser.get("https://www.google.com")
+# Find the css for the search bar
 search = browser.find_element_by_css_selector("input[name='q']")
+# Input the search term
+search.send_keys("python automation with selenium")
+# Press Google Search key
+search_btn = browser.find_element_by_css_selector("input[name='btnK']")
+search_btn.click()
